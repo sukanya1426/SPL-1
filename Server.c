49466c269a286a0f85db6a6e6f998e425c0a3819
sukanya1426 <bsse1426@iit.du.ac.bd>
2023-08-20 12:59:17 +0600

    suk

--- Server.c
+++ Server.c
@@ -1,86 +1,76 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include<winsock2.h>
+#include<ws2spi.h>
+#include<ws2tcpip.h>
 
-void error(const char *msg){
-     perror(msg);
-     exit(1);
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
 }
 
-int main(int argc,char *argv[])
-{
-    int sockfd,newsockfd,portno;
+int main(int argc, char *argv[]) {
+    int sockfd, newsockfd, portno;
     socklen_t clilen;
     char buffer[255];
-    struct sockaddr_in serv_adder,cli_adder;
+    struct sockaddr_in serv_addr, cli_addr;
     int n;
 
-    if(argc< 2){
-        fprintf(stderr," error, not port provided \n");
+    if (argc < 2) {
+        fprintf(stderr, "error, no port provided\n");
         exit(1);
     }
-    sockfd = socket(AF_INET,SOCK_STREAM,0);
 
-    if(sockfd<0){
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
         error("ERROR opening socket");
-        server = gethosebyname(argv[1]);
-
     }
-    if(server == NULL){
-        fprintf(stderr,"error,no such host\n ");
-        exit(0);
-    }
-    bzero((char*)&ser_addr,sizeof(serv_addr));
-    portno= atoi(argv[1]);
 
-    serv_adder.sin_family = AF_INET;
-    serv_adder.sin_addr.s_addr = INADDR_ANY;
-    serv_adder.sin_port = htons(portno);
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
 
-   if(bind(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr))<0)
-       error("binding Failed.");
-   
-   listen(sockfd,s);
-   clilen = sizeof(cli_adder);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
 
-   newsockfd = accept(sockfd,(struct sockaddr *)&cli_addr,&clilen);
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        error("binding Failed.");
 
-   if(newsockfd<0)
-   error("error on Accept");
+    listen(sockfd, 5); // Maximum number of queued connections
 
-   while(1)
-   {
-       bzero(buffer,255);
-       n= read(newsockfd,buffer,255);
+    clilen = sizeof(cli_addr);
 
-       if(n<0)
-           error("Error on reading . ");
+    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
 
-        printf("client : %s\n",buffer);
-        bzero(buffer,255);
-        fgets(buffer,255,stdin);
+    if (newsockfd < 0)
+        error("error on Accept");
 
-        n = write(newsockfd , buffer , strlen(buffer));
-        if(n<0)
+    while (1) {
+        bzero(buffer, 255);
+        n = read(newsockfd, buffer, 255);
+
+        if (n < 0)
+            error("Error on reading.");
+
+        printf("client: %s\n", buffer);
+        bzero(buffer, 255);
+        fgets(buffer, 255, stdin);
+
+        n = write(newsockfd, buffer, strlen(buffer));
+        if (n < 0)
             error("Error on writing.");
-        int i = strncmp("Bye",buffer,3);
-        if(i==0)
-        break;
+
+        int i = strncmp("Bye", buffer, 3);
+        if (i == 0)
+            break;
     }
 
     close(newsockfd);
     close(sockfd);
 
     return 0;
-
-
-
-
-   }
-
-
-
+}
