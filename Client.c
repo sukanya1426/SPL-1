7ea6f469b1c0b7e9108dcbb54aaddf3eed78814f
sukanya1426 <bsse1426@iit.du.ac.bd>
2023-08-21 01:10:46 -0400

    new1

--- Client.c
+++ Client.c
@@ -24,6 +24,21 @@ int main(int argc,char *argv[])
         exit(0);
     }portno = atoi(argv[2]);
     sockfd = socket(AF_INET,SOCK_STREAM,0);
-    
+    if (sockfd < 0)
+           error("Error opening socket");
+
+           server = gethostbyname(argv[1]);
+           if(server == NULL)
+           {
+            fprintf(stderr,"Error,no such host");
+
+            bzero((char* )&serv_addr,sizeof(serv_addr));
+            serv_addr.sin_family = AF_INET;
+            bcopy((char *) server->h_addr ,(char *)&serv_addr.sin_addr.s_addr , server->h_length);
+            serv_addr.sin_port = htons(portno);
+            if(connect(sockfd,(struct)))
+
+           }
+         
     return 0;
 }
