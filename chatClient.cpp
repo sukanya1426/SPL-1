f48483eddcf14e1491c14e631e086dc0244409f8
sukanya1426 <bsse1426@iit.du.ac.bd>
2023-12-13 01:45:47 -0500

    chat

---
+++ chatClient.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+using namespace std;
+
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+    string clientName;
+
+    if (argc < 3) {
+        fprintf(stderr, "usage %s hostname port\n", argv[0]);
+        exit(0);
+    }
+
+    portno = atoi(argv[2]);
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+        error("Error opening socket");
+    }
+
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr, "Error, no such host\n");
+        exit(1);
+    }
+
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        error("Connection failed");
+    }
+
+    cout << "Socket connection has been established! What is your name?: ";
+    getline(cin, clientName);
+
+    n = write(sockfd, clientName.c_str(), clientName.length());
+
+    if (n < 0) {
+        error("Error writing to socket");
+    }
+
+    // Open a file to store the conversation history
+    string historyFileName = "client_" + clientName + "_history.txt";
+    ofstream historyFile(historyFileName, ios::app);
+
+    if (!historyFile.is_open()) {
+        cerr << "Error opening the history file." << endl;
+        return 1;
+    }
+
+    cout << "You can now start chatting. Type 'Bye' to end the chat.\n";
+
+    while (true) {
+        bzero(buffer, sizeof(buffer));
+        cout << clientName << ": ";
+        fgets(buffer, sizeof(buffer), stdin);
+
+        // Write the message to the history file
+        historyFile << clientName << ": " << buffer;
+
+        n = write(sockfd, buffer, strlen(buffer));
+
+        if (n < 0) {
+            error("Error writing to socket");
+        }
+
+        if (strncmp(buffer, "Bye", 3) == 0) {
+            cout << "Ending the chat. Goodbye!\n";
+            break;
+        }
+
+        bzero(buffer, sizeof(buffer));
+        n = read(sockfd, buffer, sizeof(buffer));
+
+        if (n < 0) {
+            error("Error reading from socket");
+        }
+
+        // Write the server's response to the history file
+        historyFile << "Server: " << buffer;
+
+        cout << "Server: " << buffer;
+        
+        if (strncmp(buffer, "Bye", 3) == 0) {
+            cout << "Server has ended the chat. Goodbye!\n";
+            break;
+        }
+    }
+
+    // Close the history file
+    historyFile.close();
+
+    close(sockfd);
+    return 0;
+}
---
+++ chatServer.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+using namespace std;
+
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "port number is not provided\n");
+        fprintf(stderr, "program terminated\n");
+    }
+
+    int sockfd, newsockfd, portno, n;
+    char buffer[256];
+
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t clilen;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+        error("Error opening the socket\n");
+    }
+
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+
+    portno = atoi(argv[1]);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        error("Binding failed");
+    } else {
+        printf("Binding successful! Server is listening for connections...\n");
+    }
+
+    listen(sockfd, 1000);
+    clilen = sizeof(cli_addr);
+
+    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+
+    if (newsockfd < 0) {
+        error("Error while executing accept.\n");
+    }
+
+    char clientName[256];
+    bzero(clientName, sizeof(clientName));
+
+    // Read client's name
+    n = read(newsockfd, clientName, sizeof(clientName));
+
+    if (n < 0) {
+        error("Error reading from socket");
+    }
+
+    // Open a file to store the conversation history
+    string historyFileName = "server_" + string(clientName) + "_history.txt";
+    ofstream historyFile(historyFileName, ios::app);
+
+    if (!historyFile.is_open()) {
+        cerr << "Error opening the history file." << endl;
+        return 1;
+    }
+
+    cout << "Server is ready to chat with " << clientName << ". Type 'Bye' to end the chat.\n";
+
+    while (true) {
+        bzero(buffer, sizeof(buffer));
+        n = read(newsockfd, buffer, sizeof(buffer));
+
+        if (n < 0) {
+            error("Error reading from socket");
+        }
+
+        // Write the client's message to the history file
+        historyFile << string(clientName) << ": " << buffer;
+
+        cout << string(clientName) << ": " << buffer;
+
+        if (strncmp(buffer, "Bye", 3) == 0) {
+            cout << clientName << " has ended the chat. Goodbye!\n";
+            break;
+        }
+
+        bzero(buffer, sizeof(buffer));
+        cout << "You: ";
+        fgets(buffer, sizeof(buffer), stdin);
+
+        // Write the server's response to the history file
+        historyFile << "Server: " << buffer;
+
+        n = write(newsockfd, buffer, strlen(buffer));
+
+        if (n < 0) {
+            error("Error writing to socket");
+        }
+
+        if (strncmp(buffer, "Bye", 3) == 0) {
+            cout << "Ending the chat. Goodbye!\n";
+            break;
+        }
+    }
+
+    // Close the history file
+    historyFile.close();
+
+    close(newsockfd);
+    close(sockfd);
+
+    return 0;
+}
